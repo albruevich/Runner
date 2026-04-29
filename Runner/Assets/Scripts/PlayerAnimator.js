c41760fe29927580d58b533d84eb5a7f72ac78ca
albruevich <albruevich@gmail.com>
2026-04-29 13:59:38 +0300

    idle ainmation

--- Runner/Assets/Scripts/PlayerAnimator.js
+++ Runner/Assets/Scripts/PlayerAnimator.js
@@ -1,13 +1,20 @@
 // PlayerAnimator.js
 
 //@input Component.ScriptComponent player
+//@input Component.ScriptComponent gameManager
 //@input Component.AnimationPlayer animationPlayer
 
 var currentState = "";
 
 function initialize() {
+
     if (!script.player) {
         print("PlayerAnimator: player is not assigned.");
+        return;
+    }
+
+    if (!script.gameManager) {
+        print("PlayerAnimator: gameManager is not assigned.");
         return;
     }
 
@@ -20,7 +27,13 @@ function initialize() {
 }
 
 function updateAnimator() {
-    if (!script.player || !script.animationPlayer) {
+
+    if (!script.player || !script.gameManager || !script.animationPlayer) {
+        return;
+    }
+
+    if (script.gameManager.isGameOver || script.gameManager.isHit) {
+        playIdle();
         return;
     }
 
@@ -32,25 +45,46 @@ function updateAnimator() {
 }
 
 function playRun() {
+
     if (currentState === "Run") {
         return;
     }
 
     currentState = "Run";
 
-    script.animationPlayer.stopClip("Jump");
+    stopAll();
     script.animationPlayer.playClipAt("Run", 0);
 }
 
 function playJump() {
+
     if (currentState === "Jump") {
         return;
     }
 
     currentState = "Jump";
 
-    script.animationPlayer.stopClip("Run");
+    stopAll();
     script.animationPlayer.playClipAt("Jump", 0);
+}
+
+function playIdle() {
+
+    if (currentState === "Idle") {
+        return;
+    }
+
+    currentState = "Idle";
+
+    stopAll();
+    script.animationPlayer.playClipAt("Idle", 0);
+}
+
+function stopAll() {
+
+    script.animationPlayer.stopClip("Run");
+    script.animationPlayer.stopClip("Jump");
+    script.animationPlayer.stopClip("Idle");
 }
 
 script.createEvent("OnStartEvent").bind(initialize);
